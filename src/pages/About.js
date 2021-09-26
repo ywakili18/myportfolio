f5aff967a69030d6d1310dc84bf44deff0db157d
Yousof Wakili <ywakili18@gmail.com>
2021-09-26 16:16:03 -0700

    about page basic information displayed with color font

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,48 @@
 import React from 'react'
 
 const About = () => {
-  return <div>This is about page</div>
+  return (
+    <div>
+      <p>Hello world!</p>
+      <p>
+        I’m{' '}
+        <mark class="bg-transparent text-yellow-500">
+          Yousof Wakili, a full-stack software engineer
+        </mark>
+      </p>
+      with an emphasis on the{' '}
+      <mark class="bg-transparent text-yellow-500">front-end. </mark>
+      <p>
+        Previously, I worked in the telecom tech space wearing many different
+        hats-primarily as a{' '}
+        <mark class="bg-transparent text-yellow-500">
+          leader, communicator, and handling the day to day tasks as business
+          manager.
+        </mark>
+      </p>
+      <p>
+        Working on the client side, communicating and{' '}
+        <mark class="bg-transparent text-yellow-500">
+          understanding those of various backgrounds,
+        </mark>{' '}
+        is what inspired me becoming and pursuing my passion as a software
+        engineer.
+      </p>
+      <p>
+        I have a firm believe that techonology leads the world today and can
+        have a positive global impact. My goal is to improve the lives of those
+        are who underpriveleged by{' '}
+        <mark class="bg-transparent text-yellow-500">
+          inspiring, empowering, and creating
+        </mark>{' '}
+        tech that will improve the lives of others.
+      </p>
+      <p>
+        In my spare time, I enjoy many hobbies such as boxing, basketball, and
+        enjoying tea/coffee.
+      </p>
+    </div>
+  )
 }
 
 export default About
